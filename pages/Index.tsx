ee0f493da661b2a522d34dc9c66722ff392e6a14
masysz <67843831+masysz@users.noreply.github.com>
2024-08-07 07:33:30 +0700

    Create Index.tsx

---
+++ pages/Index.tsx
@@ -0,0 +1,15 @@
+// pages/index.tsx
+import Navbar from '../components/Navbar';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>Welcome to Next.js!</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
